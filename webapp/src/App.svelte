0f8cfdef8f508e2994ce09f34f4db01c1f864dbb
evvic <ewic98@gmail.com>
2024-03-06 17:02:00 +0700

    removed imports of .svg files

--- webapp/src/App.svelte
+++ webapp/src/App.svelte
@@ -1,18 +1,8 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
   import Counter from './lib/Counter.svelte'
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
   <h1>Vite + Svelte</h1>
 
   <div class="card">
