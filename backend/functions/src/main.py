fd57a74892aca97f8a15ae808de58ca59f927f9e
Eric <ewic98@gmail.com>
2024-03-14 20:43:08 -0700

    test update text

--- backend/functions/src/main.py
+++ backend/functions/src/main.py
@@ -5,14 +5,6 @@ import os
 # This is your Appwrite function
 # It's executed each time we get a request
 def main(context):
-    # Why not try the Appwrite SDK?
-    #
-    # client = (
-    #     Client()
-    #     .set_endpoint("https://cloud.appwrite.io/v1")
-    #     .set_project(os.environ["APPWRITE_FUNCTION_PROJECT_ID"])
-    #     .set_key(os.environ["APPWRITE_API_KEY"])
-    # )
 
     # You can log messages to the console
     context.log("Hello, Logs!")
@@ -24,7 +16,7 @@ def main(context):
     if context.req.method == "GET":
         # Send a response with the res object helpers
         # `ctx.res.send()` dispatches a string back to the client
-        return context.res.send("Hello, World!")
+        return context.res.send("Updated function!!")
 
     # `ctx.res.json()` is a handy helper for sending JSON
     return context.res.json(
