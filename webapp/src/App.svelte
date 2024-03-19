6671c789456ea293ba1dc0c9e9f051e66a717ac2
Eric <ewic98@gmail.com>
2024-03-18 19:01:49 -0700

    Ferdous chart component

--- webapp/src/App.svelte
+++ webapp/src/App.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   // import "../app.css";
-  import type { ChartProps } from "../src/components/chart.svelte"
+  import Ferdous from "./components/Ferdous.svelte"
   const key: string = import.meta.env.VITE_PUBLIC_STOCK_API_KEY;
 
   interface stock {
@@ -8,6 +8,19 @@
     price: number | void;
     pChange: number | void;
   }
+
+  const data = [
+        { time: '2019-04-11', value: 80.01 },
+        { time: '2019-04-12', value: 96.63 },
+        { time: '2019-04-13', value: 76.64 },
+        { time: '2019-04-14', value: 81.89 },
+        { time: '2019-04-15', value: 74.43 },
+        { time: '2019-04-16', value: 80.01 },
+        { time: '2019-04-17', value: 96.63 },
+        { time: '2019-04-18', value: 76.64 },
+        { time: '2019-04-19', value: 81.89 },
+        { time: '2019-04-20', value: 74.43 },
+    ];
 
   const trackedNames = [
     "Nvidia",
@@ -62,7 +75,7 @@
   </div>
 
   <div class="flex flex-col items-center"> 
-    <p>dasd</p>    
+    <Ferdous width={420} height={420} data={data}/>  
   </div>
 </main>
 
