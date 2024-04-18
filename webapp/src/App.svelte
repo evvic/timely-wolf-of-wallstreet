784433a018adeac9e2232888b0dec2fd948f1d88
jaakko <jaakko.j.virtanen@tuni.fi>
2024-04-18 15:25:03 +0300

    layout changes and added table for politician data

--- webapp/src/App.svelte
+++ webapp/src/App.svelte
@@ -11,43 +11,42 @@
     chart: {
       layout: {
         background: {
-            type: 'solid',
-            color: '#2B2B43',
+          type: "solid",
+          color: "#2B2B43",
         },
-          lineColor: '#2B2B43',
-          textColor: 'white',
-          fontWeight: 'bold',
+        lineColor: "#2B2B43",
+        textColor: "white",
+        fontWeight: "bold",
       },
       watermark: {
-          color: 'rgba(0, 0, 0, 0)',
+        color: "rgba(0, 0, 0, 0)",
       },
       crosshair: {
-          color: '#758696',
+        color: "#758696",
       },
       grid: {
         vertLines: {
-            color: '#2B2B43',
+          color: "#2B2B43",
         },
         horzLines: {
-            color: '#363C4E',
+          color: "#363C4E",
         },
       },
     },
     series: {
-        topColor: 'rgba(32, 226, 47, 0.56)',
-        bottomColor: 'rgba(32, 226, 47, 0.04)',
-        lineColor: 'rgba(32, 226, 47, 1)',
+      topColor: "rgba(32, 226, 47, 0.56)",
+      bottomColor: "rgba(32, 226, 47, 0.04)",
+      lineColor: "rgba(32, 226, 47, 1)",
     },
   };
 
   const trackedSymbols = ["QQQ", "PANW", "TSLA", "AAPL", "MSFT", "NVDA"];
 
-  // function getStocks() {
-  //   console.log(graphData);
-  // }
-  // function getStocks() {
-  //   console.log(graphData);
-  // }
+  const mockPoliticianData = [
+    { symbol: "AAPL", tradeDate: "2024-04-18", type: "SELL", amount: 1000 },
+    { symbol: "PANW", tradeDate: "2024-04-13", type: "BUY", amount: 5000 },
+    { symbol: "NVDA", tradeDate: "2024-04-09", type: "BUY", amount: 25000 },
+  ];
 
   async function fetchData() {
     graphData = [];
@@ -102,7 +101,7 @@
       Nancy's Top 5 Stocks
     </h1>
 
-    <div class="flex justify-center">
+    <div class="">
       <div class="" on:focusout={handleDropdownFocusLoss}>
         <button class="btn m-1" on:click={handleDropdownClick}>
           {#if isDropdownOpen}
@@ -121,7 +120,9 @@
               />
             </svg>
           {:else}
-            <div class="h-10 w-min bg-slate-500 rounded-xl p-2 font-bold text-white">
+            <div
+              class="h-10 w-min bg-slate-500 rounded-xl p-2 font-bold text-white"
+            >
               <p>{timeseries}🔽</p>
             </div>
           {/if}
@@ -130,7 +131,7 @@
           class="bg-slate-500 rounded-box w-min rounded-lg py-2 font-bold"
           style:visibility={isDropdownOpen ? "visible" : "hidden"}
         >
-          <li class="hover:bg-slate-800 text-left ">
+          <li class="hover:bg-slate-800 text-left">
             <button
               class="btn text-white p-1 w-full text-left"
               on:click={() => (timeseries = "DAILY")}
@@ -155,28 +156,69 @@
       </div>
     </div>
   </div>
-  <div class="">
+
+  <div>
     {#key timeseries}
       {#await fetchData()}
         <div class="flex flex-col items-center text-white">
           <p>Loading...</p>
         </div>
       {:then chart}
-        <div class="sm:grid sm:grid-rows-3 sm:grid-flow-col sm:gap-3">
+        <div
+          class="sm:grid sm:grid-rows-2 sm:grid-flow-col sm:gap-x-28 sm:gap-y-10 justify-center"
+        >
           {#each trackedSymbols as graph}
-          <div class="flex flex-col items-center">
-            <p class="text-white">{graph}</p>
-            <Ferdous
-              width={420}
-              height={420}
-              theme={THEME}
-              graphData={graphData.filter((item) => item.symbol == graph)}
-            />
-          </div>
+            <div class="bg-slate-500 p-2 rounded-lg w-min">
+              <p class="text-white font-bold text-xl">{graph}</p>
+              <Ferdous
+                width={520}
+                height={420}
+                theme={THEME}
+                graphData={graphData.filter((item) => item.symbol == graph)}
+              />
+            </div>
           {/each}
         </div>
       {/await}
     {/key}
+  </div>
+
+  <div class="flex text-center flex-col items-center pb-8">
+    <p class="text-white text-2xl font-bold mt-4">Nancy Pelosi Recent Trades</p>
+
+    <table class="table-fixed bg-slate-800 rounded-xl w-9/12 ">
+      <thead class="w-full border-b-2 border-slate-500 text-slate-200">
+        <tr>
+          <th class="">Symbol</th>
+          <th class="">Trade Date</th>
+          <th class="">Type</th>
+          <th class="">Amount</th>
+        </tr>
+      </thead>
+      <tbody class="divide-y divide-slate-500 ">
+        {#each mockPoliticianData as item}
+        <!-- border-b-2 border-slate-500 -->
+          <tr class="">
+            <td class="text-blue-300 p-4">{item.symbol}</td>
+            <td class="text-white">{item.tradeDate}</td>
+            <td class="text-white">{item.type}</td>
+            <td class="text-white">{item.amount}</td>
+
+          </tr>
+        {/each}
+      </tbody>
+
+      <!-- {#key timeseries} -->
+      <!-- needed for when there is actual data coming from API -->
+      <!-- {#await fetchData()} 
+        <div class="flex flex-col items-center text-white">
+          <p>Loading...</p>
+        </div>
+      {:then chart} -->
+
+      <!-- {/await} -->
+      <!-- {/key} -->
+    </table>
   </div>
 </main>
 
